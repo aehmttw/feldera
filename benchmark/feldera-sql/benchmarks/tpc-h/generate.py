512e9656703d51d9d8e7525adf8f3fdbc4b70b2f
Matei <matei@feldera.com>
2024-08-09 23:54:47 -0700

    update generate script csv parsing

--- benchmark/feldera-sql/benchmarks/tpc-h/generate.py
+++ benchmark/feldera-sql/benchmarks/tpc-h/generate.py
@@ -1,6 +1,7 @@
 import os
 from itertools import islice
 from plumbum.cmd import rpk
+import pandas as pd
 
 # File locations
 SCRIPT_DIR = os.path.join(os.path.dirname(__file__))
@@ -10,7 +11,10 @@ for input in ['customer', 'lineitem', 'nation', 'orders', 'part', 'partsupp', 'r
     print(input)
     data_csv = os.path.join(SCRIPT_DIR, 'data/' + input + '.csv')
 
-    with open(data_csv, 'r') as f:
-        (rpk['topic', 'create', input])()
-        for n_lines in iter(lambda: tuple(islice(f, 1000)), ()):
-            (rpk['topic', 'produce', input, '-f', '%v'] << '\n'.join(n_lines).replace(',', ';').replace(' ', '_').replace('|', ','))()
+    batch_size = 10000
+    data = pd.read_csv(data_csv, delimiter="|", header=None)
+    count = len(data.index)
+    (rpk['topic', 'create', input])()
+    for i in range(int((count - 1) / batch_size) + 1):
+        csv = data.iloc[(i * batch_size):((i + 1) * batch_size)].to_csv(index=False, header=None)
+        (rpk['topic', 'produce', input, '-f', '%v'] << csv)()
